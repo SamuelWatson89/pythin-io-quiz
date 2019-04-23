afbbbaf81ea6f6838f61053bace805de6d6ac9fc
Samuel <otacon100@hotmail.com>
2019-04-23 09:17:00 +0100

    Read and Write scripts

--- python.py
+++
@@ -1,4 +0,0 @@
-f = open('data.txt', 'r')
-lines = f.readlines()
-f.close()
-print(lines)
---
+++ quiz.py
@@ -0,0 +1,4 @@
+f = open('c:/MAMP/htdocs/Coursework/puthon-io/files/relative_data.txt', 'r')
+lines = f.read()
+f.close()
+print(lines)
